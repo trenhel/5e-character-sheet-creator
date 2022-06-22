b10b3b652f64f2fc9d4a9745432882d071eb85d9
mlehnert <trenhel27@gmail.com>
2022-06-22 15:48:02 -0500

    [] Add new font, clean up menu JS

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 let menuButton = document.querySelector('.toggleMenu');
 let dropMenu = document.querySelector('.dropMenu');
 
-function menu(){
-    if (dropMenu.style.display != 'flex'){
-        dropMenu.style.display = 'flex';
-    } else {
-        dropMenu.style.display = 'none';
-    }
-}
-
-menuButton.onclick = menu;
+menuButton.addEventListener('click', function(){
+    dropMenu.classList.toggle('change');
+})
